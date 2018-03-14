1c38d0632e7764753e2948cfe4de6a2e7b504aac
Anatoly Chernow <chertoly@gmail.com>
2018-03-14 23:44:28 +0200

    Basic handling of interdependencies

--- src/DC/Image.class.rb
+++ src/DC/Image.class.rb
@@ -1,16 +1,48 @@
+class << self
+  def all
+    @all ||= {}
+  end
+
+  def [] name
+    all[name]
+  end
+
+  def []= name, image
+    all[name] = image
+  end
+end
+
 def initialize name, directory: name
   @name = name
   @directory = directory
+  self.class[name] = self
 end
+
+attr_reader :name
 
 def exist?
   Docker::Image.exist? @name
 end
 
 def build
+  build_parent_image_if_available
   image = Docker::Image.build_from_dir @directory do |json|
     puts JSON.parse json
   end
   image.tag repo: @name
   image
 end
+
+private
+  def build_parent_image_if_available
+    dockerfile = IO.read "#{@directory}/Dockerfile"
+    from, image_name = dockerfile.each_line.first.split ' '
+    fail from unless from == 'FROM'
+
+    if image = self.class[image_name]
+      unless image.exist?
+        puts "Building parent image #{image.name}."
+        image.build
+      end
+    end
+  end
